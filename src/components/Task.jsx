5363696b5835a528141c17688fab5bf4e4fd59b3
Minh Doan <minhadoan@gmail.com>
2023-03-06 17:37:35 -0600

    Changed some css for task components

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,7 +2,7 @@ import { FaTimes } from 'react-icons/fa';
 
 const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className='task' onClick={() => onToggle(task.id)}>
+        <div className={`task ${task.reminder ? 'reminder' : ''}`} onClick={() => onToggle(task.id)}>
             <h3>{task.text} <FaTimes style={{color: 'red', cursor: 'pointer'}} onClick={() => onDelete(task.id)} /></h3>
             <p>{task.time}</p>
         </div>
