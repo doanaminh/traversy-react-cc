818c1854c87ca58a77c2d466b3dddfabd4713635
Minh Doan <minhadoan@gmail.com>
2023-03-10 16:16:55 -0600

    Added DB and code to fetch and mutate DB

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header';
 import Button from './components/Button';
@@ -8,16 +8,46 @@ import AddTask from './components/AddTask';
 const  App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+
+    getTasks();
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  }
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = {id, ...task};
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:5000/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = {id, ...task};
+    // setTasks([...tasks, newTask]);
   }
 
   // Delete Task
-  const deleteTask = id => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method:'DELETE',
+    })
+
     setTasks(tasks.filter(el => el.id !== id));
   }
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,8 +3,8 @@ import Task from './Task';
 const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
-            {tasks.map(el => {
-                return <Task key={el.id} task={el} onDelete={onDelete} onToggle={onToggle} />
+            {tasks.map((el, i) => {
+                return <Task key={i} task={el} onDelete={onDelete} onToggle={onToggle} />
             }
             )}
         </>
